51fda557f39d5268c9567b24f50cedee5bb02ed4
KerenLopez <kerenlopezcordoba@outlook.es>
2021-09-25 01:03:06 -0500

    The method showWelcomeWindow was created

--- src/model/VideogameStoreGUI.java
+++ src/model/VideogameStoreGUI.java
@@ -2,28 +2,35 @@ package model;
 
 import java.io.IOException;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.layout.BorderPane;
 
 public class VideogameStoreGUI {
-	
+
 	private VideogameStore videogame;
-	
+
 	@FXML
 	private BorderPane mainPane;
-	
+
 	public VideogameStoreGUI(VideogameStore v) {
 		videogame = v;
 	}
-	
+
 	public void showWelcomeWindow() throws IOException {
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("welcome.fxml"));
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/welcome.fxml"));
 		fxmlLoader.setController(this);
 		Parent menuPane = fxmlLoader.load();
 		mainPane.getChildren().clear();
 		mainPane.setCenter(menuPane);
 		mainPane.setStyle("-fx-background-image: url(/ui/fondo1.jpg)");
 	}
+
+	@FXML
+	public void nextScreen(ActionEvent event) {
+
+	}
+
 }
