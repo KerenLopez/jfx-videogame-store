8f023ac19dbc9b2d4f1909c3831efe12a887860c
AngelicaCorrales <angelicacorrales2002@gmail.com>
2021-09-25 10:18:01 -0500

    GUI modified: action of buttons

--- src/model/VideogameStoreGUI.java
+++ src/model/VideogameStoreGUI.java
@@ -6,6 +6,9 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 
 public class VideogameStoreGUI {
@@ -14,6 +17,30 @@ public class VideogameStoreGUI {
 
 	@FXML
 	private BorderPane mainPane;
+
+
+
+	@FXML
+	private TextField txtNumCashiers;
+
+	@FXML
+	private TextField txtNumShelves;
+
+
+	@FXML
+	private TextField txtIndicator;
+
+	@FXML
+	private TextField txtNumVideogames;
+
+	@FXML
+	private TableView<?> tvShelves;
+
+	@FXML
+	private TableColumn<?, ?> colIndicator;
+
+	@FXML
+	private TableColumn<?, ?> colNumVideogames;
 
 	public VideogameStoreGUI(VideogameStore v) {
 		videogame = v;
@@ -31,6 +58,73 @@ public class VideogameStoreGUI {
 	@FXML
 	public void nextScreen(ActionEvent event) {
 
+	}
+
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	@FXML
+	public void nextShelves(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/shelves.fxml"));
+		fxmlLoader.setController(this);
+		Parent menuPane = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.setCenter(menuPane);
+		mainPane.setStyle("-fx-background-image: url(/ui/fondo2.jpg)");
+	}
+
+
+
+	@FXML
+	public void add(ActionEvent event) {
+
+	}
+
+	@FXML
+	public void nextAddGame(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/ui/addGame.fxml"));
+		fxmlLoader.setController(this);
+		Parent menuPane = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.setCenter(menuPane);
+		mainPane.setStyle("-fx-background-image: url(/ui/fondo2.jpg)");
 	}
 
 }
