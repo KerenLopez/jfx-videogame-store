58fdd74979a934666b02e56c384c332578cda930
KerenLopez <kerenlopezcordoba@outlook.es>
2021-09-25 10:16:32 -0500

    New methods were added to VideogameStoreGUI

--- src/model/VideogameStoreGUI.java
+++ src/model/VideogameStoreGUI.java
@@ -6,6 +6,8 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.BorderPane;
 
 public class VideogameStoreGUI {
@@ -14,6 +16,33 @@ public class VideogameStoreGUI {
 
 	@FXML
 	private BorderPane mainPane;
+
+	@FXML
+	private TableView<?> tvS2;
+
+	@FXML
+	private TableColumn<?, ?> idCol;
+
+	@FXML
+	private TableColumn<?, ?> listgamesCol;
+
+	@FXML
+	private TableColumn<?, ?> timeCol;
+
+	@FXML
+	private TableView<?> tvS3;
+
+	@FXML
+	private TableColumn<?, ?> basketCol;
+	
+    @FXML
+    private TableView<?> tvS4;
+
+    @FXML
+    private TableColumn<?, ?> bagCol;
+
+    @FXML
+    private TableColumn<?, ?> totalpriceCol;
 
 	public VideogameStoreGUI(VideogameStore v) {
 		videogame = v;
@@ -29,8 +58,82 @@ public class VideogameStoreGUI {
 	}
 
 	@FXML
-	public void nextScreen(ActionEvent event) {
-
+	public void startSimulation(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("numberCashiersNShelves.fxml"));
+		fxmlLoader.setController(this);
+		Parent menuPane = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.setCenter(menuPane);
+		mainPane.setStyle("-fx-background-image: url(/ui/fondo2.jpg)");
 	}
+
+	@FXML
+	public void goToSection3(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("section3.fxml"));
+		fxmlLoader.setController(this);
+		Parent menuPane = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.setCenter(menuPane);
+		mainPane.setStyle("-fx-background-image: url(/ui/fondo2.jpg)");
+	}
+
+	@FXML
+    public void goToSection4(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("section4.fxml"));
+		fxmlLoader.setController(this);
+		Parent menuPane = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.setCenter(menuPane);
+		mainPane.setStyle("-fx-background-image: url(/ui/fondo2.jpg)");
+    }
+	
+	@FXML
+    public void goToEndingScreen(ActionEvent event) throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("ending.fxml"));
+		fxmlLoader.setController(this);
+		Parent menuPane = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.setCenter(menuPane);
+		mainPane.setStyle("-fx-background-image: url(/ui/fondo2.jpg)");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
