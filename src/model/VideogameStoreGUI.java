0b89dfd44119a91e13d594bddb9f70ad3aff6485
KerenLopez <kerenlopezcordoba@outlook.es>
2021-09-25 00:21:16 -0500

    Methods in Main class were added, also the class VideogameStoreGUI was created

---
+++ src/model/VideogameStoreGUI.java
@@ -0,0 +1,29 @@
+package model;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.BorderPane;
+
+public class VideogameStoreGUI {
+	
+	private VideogameStore videogame;
+	
+	@FXML
+	private BorderPane mainPane;
+	
+	public VideogameStoreGUI(VideogameStore v) {
+		videogame = v;
+	}
+	
+	public void showWelcomeWindow() throws IOException {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("welcome.fxml"));
+		fxmlLoader.setController(this);
+		Parent menuPane = fxmlLoader.load();
+		mainPane.getChildren().clear();
+		mainPane.setCenter(menuPane);
+		mainPane.setStyle("-fx-background-image: url(/ui/fondo1.jpg)");
+	}
+}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,10 +1,40 @@
 package ui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import model.VideogameStore;
+import model.VideogameStoreGUI;
 
+public class Main extends Application{
+
+	private VideogameStore videogame;
+	private VideogameStoreGUI videogameGUI;
+	
+	public Main() {
+		videogame= new VideogameStore();
+		videogameGUI= new VideogameStoreGUI(videogame);	
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainPane.fxml"));
+		
+		fxmlLoader.setController(videogameGUI);
+		Parent root= fxmlLoader.load();
+		
+		Scene scene= new Scene(root);
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Tienda de videojuegos");
+		primaryStage.show();
+		
 	}
 
 }
