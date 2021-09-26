99f30f77bd9e1c811b7cece6f609594b3e832e35
AngelicaCorrales <angelicacorrales2002@gmail.com>
2021-09-26 12:32:03 -0500

    Test modified

---
+++ test/dataStructures/HashTableTest.java
@@ -0,0 +1,20 @@
+package dataStructures;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import model.Videogame;
+
+class HashTableTest {
+	
+	public void setupScenary1() {
+		HashTable<Integer, Videogame> shelf=new HashTable<>(7);
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
